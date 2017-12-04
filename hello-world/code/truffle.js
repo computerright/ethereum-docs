6ea8e030f036a276ca0a9147f3febc0a69061f45
Errol Chevannes <errol@malooba.com>
2017-12-04 11:54:55 +0000

    Updated hello-world truffle config with defaults for private-blockchain.

--- hello-world/code/truffle.js
+++ hello-world/code/truffle.js
@@ -1,4 +1,14 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+   
+    // Uncomment this when you want to run truffle on the private-blockchain
+    // networks: {
+    //   development: {
+    //     host: "localhost",
+    //     port: 8545,
+    //     network_id: "100",
+    //     gas: 2000000
+    //   }
+    // }
+  };
