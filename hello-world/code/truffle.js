411bd30cfee667b24a356b99e7694b815f9ff3fd
Kai Davenport <kaiyadavenport@gmail.com>
2017-12-06 21:15:30 +0000

    update local deploy

--- hello-world/code/truffle.js
+++ hello-world/code/truffle.js
@@ -2,13 +2,16 @@ module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // to customize your Truffle configuration!
    
+    /*
     // Uncomment this when you want to run truffle on the private-blockchain
-    // networks: {
-    //   development: {
-    //     host: "localhost",
-    //     port: 8545,
-    //     network_id: "100",
-    //     gas: 2000000
-    //   }
-    // }
+
+    */
+    networks: {
+      development: {
+        host: "localhost",
+        port: 8545,
+        network_id: "23422",
+        gas: 2000000
+      }
+    }
   };
